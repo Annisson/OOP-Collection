5b04e1cd7051cc39674efe0ca5dccdab9591f20c
Annica Andersson <annicaandersson@live.se>
2024-03-07 14:29:55 +0100

    Add project files.

---
+++ OOP-Collection/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOP_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OOP - Incapsulation, Inheritance, and Polymorphism");
+
+
+        }
+    }
+}
