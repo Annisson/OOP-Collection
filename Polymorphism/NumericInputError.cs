e84e7f87a91c9c1dfb194bff8a09cfacc06c5308
Annica Andersson <annicaandersson@live.se>
2024-03-09 05:27:55 +0100

    Add NumericInputError class

---
+++ Polymorphism/NumericInputError.cs
@@ -0,0 +1,10 @@
+﻿namespace Polymorphism
+{
+    internal class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a numeric input in a text only field. This fired an error!";
+        }
+    }
+}
