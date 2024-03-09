1b76acae9808a02d463a988875ddbcc251f5bbe3
Annica Andersson <annicaandersson@live.se>
2024-03-09 20:26:07 +0100

    Move questions from Animal.cs to Program.cs

--- Inheritance/Animal.cs
+++ Inheritance/Animal.cs
@@ -16,13 +16,5 @@
             Weight = weight;
             CanIPetThatDawg = pet;
         }
-
-
-        // 13. F: Om vi under utvecklingen kommer fram till att samtliga fåglar behöver ett nytt attribut, i vilken klass bör vi lägga det?
-        // S: I klassen Bird, eftersom alla fåglar ärver från den och därmed får med sig allt som bygger basen för en fågel
-
-        // 14. F: Om alla djur behöver det nya attributet, vart skulle man lägga det då?
-        // S: I klassen Animal som alla djuren ärver ifrån (förutsatt att vi implementerat arvet)
-
     }
 }
