a8a84b6a4b8bbd6ccfb40e023447814e4bbb4de5
Annica Andersson <annicaandersson@live.se>
2024-03-09 20:12:16 +0100

    Add questions and full constructor

--- Inheritance/Animal.cs
+++ Inheritance/Animal.cs
@@ -5,14 +5,24 @@
         public string Name { get; set; }
         public int Age { get; set; }
         public double Weight { get; set; }
-        public int CanIPetThatDawg { get; set; }
+        public bool CanIPetThatDawg { get; set; }
 
         public abstract void DoSound();
 
-        protected Animal()
+        protected Animal(string name, int age, double weight, bool pet)
         {
-
+            Name = name;
+            Age = age;
+            Weight = weight;
+            CanIPetThatDawg = pet;
         }
+
+
+        // 13. F: Om vi under utvecklingen kommer fram till att samtliga fåglar behöver ett nytt attribut, i vilken klass bör vi lägga det?
+        // S: I klassen Bird, eftersom alla fåglar ärver från den och därmed får med sig allt som bygger basen för en fågel
+
+        // 14. F: Om alla djur behöver det nya attributet, vart skulle man lägga det då?
+        // S: I klassen Animal som alla djuren ärver ifrån (förutsatt att vi implementerat arvet)
 
     }
 }
