5d1848f1368758bfbdb668e9933ee154b92252ff
Annica Andersson <annicaandersson@live.se>
2024-03-09 05:29:15 +0100

    Test NumericInputError and TextInputError classes

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -6,7 +6,20 @@
         {
             Console.WriteLine("3.2 - Polymorphism ");
 
+            List<UserError> errorList = new List<UserError>();
 
+            NumericInputError numErr = new NumericInputError();
+            errorList.Add(numErr);
+
+            TextInputError textErr = new TextInputError();
+            errorList.Add(textErr);
+
+
+
+            foreach (UserError error in errorList)
+            {
+                Console.WriteLine($"Error: {error.UEMessage()}");
+            }
 
         }
     }
