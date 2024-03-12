8cb01e6ccb31dfbe2bda149b6cc8c0705f856fca
Annica Andersson <annicaandersson@live.se>
2024-03-12 11:03:59 +0100

    Implement Stats-method to Bird classes

--- Inheritance/Bird.cs
+++ Inheritance/Bird.cs
@@ -1,4 +1,6 @@
-﻿namespace Inheritance
+﻿using System.Drawing;
+
+namespace Inheritance
 {
     internal class Bird : Animal
     {
@@ -7,55 +9,71 @@
         {
             Console.WriteLine("You're trying to sleep? Let me sing you the song of my people");
         }
-        public Bird(string name, int age, double weight, bool pet, bool food) : base(name, age, weight, pet)
+        public Bird(string name, int age, double weight, bool pet, bool theif) : base(name, age, weight, pet)
         {
             Name = name;
             Age = age;
             Weight = weight;
             CanIPetThatDawg = pet;
-            WillStealFood = food;
+            WillStealFood = theif;
+        }
+        public override string Stats()
+        {
+            return base.Stats() + $", Will steal food? {(WillStealFood ? "Big theif" : "No way!")}";
         }
     }
 
     internal class Pelican : Bird
     {
         public int BeakLength { get; set; }
-        public Pelican(string name, int age, double weight, bool pet, bool food, int beak) : base(name, age, weight, pet, food)
+        public Pelican(string name, int age, double weight, bool pet, bool theif, int beak) : base(name, age, weight, pet, theif)
         {
             Name = name;
             Age = age;
             Weight = weight;
             CanIPetThatDawg= pet;
-            WillStealFood = food;
+            WillStealFood = theif;
             BeakLength = beak;
+        }
+        public override string Stats()
+        {
+            return base.Stats() + $", Beak length: {BeakLength}cm";
         }
     }
 
     internal class Flamingo : Bird
     {
         public string Color { get; set; }
-        public Flamingo(string name, int age, double weight, bool pet, bool food, string color) : base(name, age, weight, pet, food)
+        public Flamingo(string name, int age, double weight, bool pet, bool theif, string color) : base(name, age, weight, pet, theif)
         {
             Name = name;
             Age = age;
             Weight = weight;
             CanIPetThatDawg = pet;
-            WillStealFood = food;
+            WillStealFood = theif;
             Color = color;
+        }
+        public override string Stats()
+        {
+            return base.Stats() + $", Color: {Color}";
         }
     }
 
     internal class Swan : Bird
     {
         public bool IsMean { get; set; }
-        public Swan(string name, int age, double weight, bool pet, bool food, bool mean) : base(name, age, weight, pet, food)
+        public Swan(string name, int age, double weight, bool pet, bool theif, bool mean) : base(name, age, weight, pet, theif)
         {
             Name = name;
             Age = age;
             Weight = weight;
             CanIPetThatDawg = pet;
-            WillStealFood= food;
+            WillStealFood= theif;
             IsMean = mean;
+        }
+        public override string Stats()
+        {
+            return base.Stats() + $", Mean birdie? {(IsMean ? "Very mean :(" : "Very kind, promise")}";
         }
     }
 }
