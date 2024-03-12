1a2c116ac9ba0c88db65ce07efce63ff610a15c3
Annica Andersson <annicaandersson@live.se>
2024-03-12 09:52:26 +0100

    Change Dog prop

--- Inheritance/Dog.cs
+++ Inheritance/Dog.cs
@@ -2,18 +2,18 @@
 {
     internal class Dog : Animal
     {
-        public bool HasGoodTeef { get; set; }
+        public int NumberOfTeef { get; set; }
         public override void DoSound()
         {
-            Console.WriteLine("Woof woof ruff");
+            Console.WriteLine("Rrrrrrrruff");
         }
-        public Dog(string name, int age, double weight, bool pet, bool teef) : base(name, age, weight, pet)
+        public Dog(string name, int age, double weight, bool pet, int teef) : base(name, age, weight, pet)
         {
             Name = name;
             Age = age;
             Weight = weight;
             CanIPetThatDawg = pet;
-            HasGoodTeef = teef;
+            NumberOfTeef = teef;
         }
     }
 }
