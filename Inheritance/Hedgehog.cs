073de74253c7b7e9bb9bec9b5863755a2627d814
Annica Andersson <annicaandersson@live.se>
2024-03-12 09:58:59 +0100

    Change Hedgehog prop

--- Inheritance/Hedgehog.cs
+++ Inheritance/Hedgehog.cs
@@ -2,18 +2,18 @@
 {
     internal class Hedgehog : Animal
     {
-        public bool IsCute { get; set; }
+        public int WinsAgainstCats { get; set; }
         public override void DoSound()
         {
             Console.WriteLine("Squeaky squeak eek eek");
         }
-        public Hedgehog(string name, int age, double weight, bool pet, bool cute) : base(name, age, weight, pet)
+        public Hedgehog(string name, int age, double weight, bool pet, int wins) : base(name, age, weight, pet)
         {
             Name = name;
             Age = age;
             Weight = weight;
             CanIPetThatDawg = pet;
-            IsCute = cute;
+            WinsAgainstCats = wins;
         }
     }
 }
